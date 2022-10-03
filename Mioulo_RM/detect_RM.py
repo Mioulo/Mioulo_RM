c863591551c77595266ccb30cfb7edbf5294679f
mioulo <1669075866@qq.com>
2022-10-03 22:09:37 +0800

    Signed-off-by: mioulo <1669075866@qq.com>

--- Mioulo_RM/detect_RM.py
+++ Mioulo_RM/detect_RM.py
@@ -148,8 +148,8 @@ def detect(save_img=False):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--weights', nargs='+', type=str, default='D:\\Pyworkprojects\\yolov5-5.0\\runs\\train\\exp6\\weights\\best.pt', help='model.pt path(s)')
-    parser.add_argument('--source', type=str, default='D:\\Pyworkprojects\\yolov5-5.0\\data_RM\\dect', help='source')  # file/folder, 0 for webcam
+    parser.add_argument('--weights', nargs='+', type=str, default='', help='model.pt path(s)')
+    parser.add_argument('--source', type=str, default='', help='source')  # file/folder, 0 for webcam
     parser.add_argument('--img-size', type=int, default=640, help='inference size (pixels)')
     parser.add_argument('--conf-thres', type=float, default=0.6, help='object confidence threshold')
     parser.add_argument('--iou-thres', type=float, default=0.2, help='IOU threshold for NMS')
